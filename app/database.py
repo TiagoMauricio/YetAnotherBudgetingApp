09a1941a78f6dd42962b151697a135a184793772
Tiago <tiagolcmauricio@gmail.com>
2025-10-24 21:07:14 +0100

    Clean requirements, format

--- app/database.py
+++ app/database.py
@@ -13,11 +13,15 @@ connect_args = (
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./db.sqlite3")
 
 engine = create_engine(DATABASE_URL, connect_args=connect_args)
+
+
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
         yield session
+
 
 SessionDependency = Annotated[Session, Depends(get_session)]
