9672b841c4534dada117631954bdff177ad4ed9d
Tiago <tiagolcmauricio@gmail.com>
2025-12-30 09:27:00 +0000

    Change update transaction method flow

--- app/routes/transactions.py
+++ app/routes/transactions.py
@@ -6,7 +6,7 @@ from app.database import get_session
 from app.utils.dependencies import get_current_user
 from app.schemas.transactions import TransactionResponse, TransactionUpdate
 from app.crud import transactions as t_crud, accounts as acc_crud
-
+from app.models import Transaction
 from typing import Annotated
 
 router = APIRouter(tags=["transactions"])
@@ -56,8 +56,8 @@ async def update_transaction(
     transaction_data: TransactionUpdate,
     user: Annotated[str, Depends(get_current_user)],
     session: Session = Depends(get_session),
-) -> TransactionResponse:
-    transaction: TransactionResponse | None = t_crud.update_transaction(transaction_data, user, session)
+) -> TransactionResponse | None:
+    transaction: Transaction | None = t_crud.find_transaction_by_id(transaction_data.transaction_id, session)
     if not transaction:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -68,7 +68,8 @@ async def update_transaction(
             status_code=status.HTTP_403_FORBIDDEN,
             detail="User does not have acccess to account."
         )
-    return transaction
+    updated_transaction: TransactionResponse | None = t_crud.update_transaction(transaction_data, user, session)
+    return updated_transaction
 
 
 @router.delete("/{transaction_id")
